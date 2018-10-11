dd2f5b8d90c1fab82fbdd7f886b35b7af3621266
Przemek <kasprzakprzemyslaw417@gmail.com>
2018-10-11 12:05:59 +0200

    Scheduler implementation

---
+++ Laboratoria/Assets/Scripts/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Scheduler._instance.ScheduleParameter(5);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Laboratoria/Assets/Scripts/Scheduler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scheduler : MonoBehaviour
+{
+    public static Scheduler _instance { get; private set; }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ScheduleParameter (float delay, string message)
+    {
+        Debug.Log("Delay is: " + delay);
+    }
+
+    private IEnumerator RunDelayed(float delay, string message)
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("DelayedAttribute message: " + message);
+    }
+}
+
