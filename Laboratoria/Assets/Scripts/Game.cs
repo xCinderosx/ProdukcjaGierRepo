0ed110a2e8247412a67d61e984679dce3731901d
Przemek <kasprzakprzemyslaw417@gmail.com>
2018-10-11 12:31:40 +0200

    Scheduler active function and delegate sample

--- Laboratoria/Assets/Scripts/Game.cs
+++ Laboratoria/Assets/Scripts/Game.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Game : MonoBehaviour {
 
+    public float delay;
+    public List<GameObject> prefabTargets;
+
 	// Use this for initialization
 	void Start () {
-        Scheduler._instance.ScheduleParameter(5);
+        Action a = () =>
+        {
+            
+            foreach (var item in prefabTargets)
+            { 
+                item.active = false;
+            }
+
+        };
+        Scheduler._instance.ScheduleParameter(delay, a);
     }
 	
 	// Update is called once per frame
--- Laboratoria/Assets/Scripts/Scheduler.cs
+++ Laboratoria/Assets/Scripts/Scheduler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Scheduler : MonoBehaviour
 {
@@ -20,15 +21,17 @@ public class Scheduler : MonoBehaviour
 		
 	}
 
-    public void ScheduleParameter (float delay, string message)
+    public void ScheduleParameter (float delay, Action action)
     {
         Debug.Log("Delay is: " + delay);
+        StartCoroutine(RunDelayed(delay, action));
+        
     }
 
-    private IEnumerator RunDelayed(float delay, string message)
+    private IEnumerator RunDelayed(float delay, Action action)
     {
         yield return new WaitForSeconds(delay);
-        Debug.Log("DelayedAttribute message: " + message);
+        action();
     }
 }
 
